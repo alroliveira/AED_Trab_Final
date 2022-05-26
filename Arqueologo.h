4cc04b978482a4bdd92c51477c8e79eeeb2f119b
alroliveira <alr.oliveira@campus.fct.unl>
2022-05-26 16:06:32 +0100

    a.h

--- Arqueologo.h
+++ Arqueologo.h
@@ -10,12 +10,40 @@ typedef struct _terreno * terreno;
 
 /* Prototipos das funcoes associadas ao Arqueologo */
 
+/***********************************************
+criarArqueologo - Criacao da instancia da estrutura associada ao Arqueologo.
+Parametros:
+	nome - nome do arqueologo
+Retorno: apontador para a  instancia criada
+Pre-condicoes:
+***********************************************/
 arqueologo criarArqueologo(char *nome); 
 
+/***********************************************
+calcularMeritoArqueologo - Calcula o merito do arqueologo.
+Parametros:
+	a - arqueologo
+Retorno: merito do arqueologo
+Pre-condicoes: a != NULL
+***********************************************/
 int calcularMeritoArqueologo(arqueologo a);
 
+/***********************************************
+daNomeArqueologo - Consulta o nome da estrutura associada ao Arqueologo
+Parametros: 
+	a - arqueologo
+Retorno: nome do arqueologo
+Pre-condicoes: a != NULL
+***********************************************/
 char *daNomeArqueologo(arqueologo a);
 
+/***********************************************
+destroiArqueologo - Liberta a memoria ocupada pela instancia da estrutura associada ao arqueologo.
+Parametros:
+	a - arqueologo a destruir
+Retorno:
+Pre-condicoes: a != NULL
+***********************************************/
 void destroiArqueologo(arqueologo a);
 
 void destroiGenArqueologo(void * a);
