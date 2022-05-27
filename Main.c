ecd933b9f1f7dfc05446dcc110fdfbd570cffb58
Rodrigo <rodrigorfonseca@outlook.com>
2022-05-27 19:00:26 +0100

    -

--- Main.c
+++ Main.c
@@ -34,7 +34,7 @@ void iniciarConcurso(concurso c);
     void cmdEquipa(concurso c, int i);
     void cmdEscavacao(concurso c, int lSalto, int cSalto, char *nome);
     void cmdSair(concurso c);
-    void preencheTerreno(concurso c,int linha,int coluna)
+    void preencheTerreno(concurso c,int linha,int coluna);
 
 
 //Funcoes
@@ -58,7 +58,7 @@ void iniciarConcurso(concurso c)
     c = criaConcurso();
     lerTerreno(c,linha,coluna);
     atribuiLCTerreno(linha,coluna, T);
-    lerTerreno(c,linha,coluna);
+    preencheTerreno(c,linha,coluna);
     lerEquipas(c);
 }
 
