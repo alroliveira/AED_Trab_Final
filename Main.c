a191d45d179db1a5474619e95d6363bceb5634f5
Rodrigo <rodrigorfonseca@outlook.com>
2022-05-27 18:57:09 +0100

    -

--- Main.c
+++ Main.c
@@ -34,6 +34,7 @@ void iniciarConcurso(concurso c);
     void cmdEquipa(concurso c, int i);
     void cmdEscavacao(concurso c, int lSalto, int cSalto, char *nome);
     void cmdSair(concurso c);
+    void lerTerreno(concurso c,int linha,int coluna)
 
 
 //Funcoes
